aab70c1bd36e66b7013178bce2a7a3d71bfe78db
Austin Diaz <abd5815@durga.bx.psu.edu>
2022-11-27 21:32:12 -0500

    fix script

--- src/jobs/run_variant_loc.sh
+++ src/jobs/run_variant_loc.sh
@@ -5,9 +5,10 @@
 #SBATCH -o out.log
 #SBATCH -e error.log
 #SBATCH --time=4:00:00
+#SBATCH --cpus-per-task=20
 #SBATCH --mem-per-cpu=256000
 #SBATCH --chdir /data5/austin/work/variant_loc/variant_loc/src
 #SBATCH --nodelist ramona
-#SBATCH --ntasks 20
-
+#SBATCH --ntasks 1
+#SBATCH --nodes 1
 python find_gene.py
